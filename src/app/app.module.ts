5a0c11fdceefeb4e46bd7d583233f8fd6ea33be7
Blockost <simon.espigole@gmail.com>
2020-02-06 10:34:45 +0100

    feat: configure angular app to work with Phaser + implement hello world scene

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,14 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppComponent } from './app.component';
+import { AppComponent } from "./app.component";
+import { GameComponent } from "./game/game/game.component";
+import { HelloWorldSceneComponent } from "./scenes/hello-world-scene/hello-world-scene.component";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, GameComponent, HelloWorldSceneComponent],
+  imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
