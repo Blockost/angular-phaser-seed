9e37473868a61ff5ffcb3a95d254bb538b3088a5
Blockost <simon.espigole@gmail.com>
2020-02-06 16:31:43 +0100

    chore: fix linting + test + remove e2e

--- src/app/scenes/hello-world-scene/hello-world-scene.component.ts
+++ src/app/scenes/hello-world-scene/hello-world-scene.component.ts
@@ -25,16 +25,14 @@ export class HelloWorldSceneComponent extends BaseScene {
 
     this.add.image(400, 300, 'sky');
 
-    var particleEmitterManager = this.add.particles('red');
-
-    var emitter = particleEmitterManager.createEmitter({
+    const particleEmitterManager = this.add.particles('red');
+    const emitter = particleEmitterManager.createEmitter({
       speed: 100,
       scale: { start: 1, end: 0 },
       blendMode: 'ADD'
     });
 
-    var logo = this.physics.add.image(100, 100, 'logo');
-
+    const logo = this.physics.add.image(100, 100, 'logo');
     logo.setVelocity(100, 200);
     logo.setBounce(1, 1);
     logo.setCollideWorldBounds(true);
